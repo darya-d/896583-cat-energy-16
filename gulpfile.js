9ef6f84424d4e15347492de4249c91bda6c2ea0b
Darya <darya.drazdouskaya@gmail.com>
2019-05-27 12:59:37 +0300

    add gulp assemble

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,10 @@ var csso = require('gulp-csso'); // минификация css
 var rename = require("gulp-rename"); // переименование
 var imagemin = require("gulp-imagemin"); // оптимизация img
 var webp = require("gulp-webp"); //отптимизация img - создания формата .webp
+var svgstore = require("gulp-svgstore"); // сборка svg спрайта
+var posthtml = require("gulp-posthtml"); // автоматизация html
+var include = require("posthtml-include"); // тег в html <include>
+var del = require("del"); // удаление файлов перед копированием
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
@@ -23,13 +27,13 @@ gulp.task("css", function () {
     .pipe(csso())
     .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
 gulp.task("server", function () {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -37,9 +41,11 @@ gulp.task("server", function () {
   });
 
   gulp.watch("source/less/**/*.less", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
+  gulp.watch("source/*.html", gulp.series("html", "refresh"));
 });
 
+// оптимизация img
 gulp.task("images", function () {
   return gulp.src("source/img/*.{png,jpg,svg}")
     .pipe(imagemin([
@@ -50,10 +56,65 @@ gulp.task("images", function () {
     .pipe(gulp.dest("source/img"));
 });
 
+//отптимизация img - создание формата .webp
 gulp.task("webp", function () {
   return gulp.src("source/img/*.{png,jpg}")
     .pipe(webp({quality: 90}))
     .pipe(gulp.dest("source/img"));
 });
 
-gulp.task("start", gulp.series("css", "server"));
+// сборка svg спрайта
+gulp.task("sprite", function () {
+  return gulp.src("source/img/*{icon-vk,icon-insta,icon-fb,icon-mail,icon-phone,htmlacademy}.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"));
+});
+
+// автоматизация html
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest("build"));
+});
+
+// Копирование файлов в папку build  
+gulp.task("copy", function () {
+  return gulp.src([
+      "source/fonts/**/*.{woff,woff2}",
+      "source/img/**",
+      "source/*.ico"
+    ], {
+      base: "source"
+    })
+    .pipe(gulp.dest("build"));
+});
+
+// удаление файлов перед копированием
+gulp.task("clean", function () {
+  return del("build");
+});
+
+// Автообновление странички
+gulp.task("refresh", function (done) {
+  server.reload();
+  done();
+});
+
+// Метатаски - алиасы
+gulp.task("build", gulp.series(
+  "clean",
+  "copy",
+  "css",
+  "sprite",
+  "html"
+));
+
+gulp.task("start", gulp.series(
+  "build", 
+  "server"
+));
